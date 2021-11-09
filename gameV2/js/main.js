593fac84ed88dc7a895c2bbaccbaf4becaca42f5
nimsite <91060664+nimsite2@users.noreply.github.com>
2021-11-09 23:03:35 +0900

    Add files via upload

--- gameV2/js/main.js
+++ gameV2/js/main.js
@@ -6,6 +6,7 @@ cvs.height = 480;
 var cnt = 0;
 var game = 1;
 var time = 0;
+var frame = 0;
 var cc = cnt;
 var cg = game;
 /*Canvas*/
@@ -34,6 +35,7 @@ var deathcnt = 0;
 var cx = x;
 var cy = y;
 var cs = 0;
+var cvy = vy;
 var spawn=true;
 var playerturn=0;
 var hitboxX = 20;
@@ -43,38 +45,26 @@ var hitboxY = 25;
 /*enemy*/
 var enemy = new Image();
 enemy.src = "./obj/enemy.png";
-var ex = 0;
-var ey = 0;
-var cex = ex;
-var cey = ey;
-var evy = 0;
-var cevy = evy;
-var ejump=true;
-var turn=0;
-var cet=turn;
-var espeed = 0.7;
-var ces = espeed;
+var espeed = new Array();
+var enemyX = new Array();//
+var enemyY = new Array();//
+var enemyturn = new Array();
+var enemyjump = new Array();
+var enemyV = new Array();
+var cejump = new Array();
+var ceturn = new Array();
+var cex = new Array();
+var cey = new Array();
+var cev = new Array();
+var ces = new Array();
+var espawn = true;
 /*enemy*/
-
-/*enemy2*/
-var enemy2 = new Image();
-enemy2.src = "./obj/enemy2.png";
-var ex2 = 0;
-var ey2 = 0;
-var cex2 = ex;
-var cey2 = ey;
-var evy2 = 0;
-var cevy2 = evy2;
-var ejump2=true;
-var turn2=0;
-var cet2=turn2;
-var espeed2 = 0.7;
-var ces2 = espeed2;
-/*enemy2*/
 
 /*bg*/
 var background = new Image();
+var background1 = new Image();
 background.src = "./obj/background.png";
+background1.src = "./obj/background1.png";
 /*bg*/
 
 /*Map*/
@@ -91,32 +81,32 @@ makemap(screen);
 
 function makemap(s) {
 if (s==0){
-once = true;
-background.src = "./obj/background.png";
+once=true;
+espawn = true;
 mapchip.src = "./obj/tile.png";
 map = [
 //   -0-----------------------------1-----------------------------2--------------------------
 //   -0--1--2--3--4--5--6--7--8--9--0--1--2--3--4--5--6--7--8--9--0--1--2--3--4--5--6--7--8
 	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//00
 	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//01
-	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//02
+	[00,00,00,00,00,00,00,00,00,11,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//02
 	[22,21,24,23,26,25,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//03
-	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//04
-	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//05
+	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,11,00,00,00,00,00,00,00,00,00],//04
+	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,11,00,00,00,00,00,00,00,00,00,00,00,00,00],//05
 	[00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00],//06
 	[08,00,00,00,00,00,09,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,22,00,00,00,00,00],//07
-	[05,05,05,05,65,05,05,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,22,00,00,00,00,00],//08
+	[05,05,05,05,65,05,05,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,22,00,00,00,00,22],//08
 	[00,00,00,00,06,00,00,00,00,00,00,00,00,00,00,28,28,28,00,00,30,00,00,00,22,00,00,00,00,00],//09
 	[03,00,00,00,06,00,00,00,00,00,00,00,00,00,00,27,27,27,00,00,29,00,00,00,22,00,00,00,00,00],//10
 	[01,00,00,00,06,00,00,05,05,05,05,05,05,05,00,27,27,27,00,00,29,00,00,00,22,00,00,00,00,00],//11
-	[01,00,10,00,06,00,00,22,00,20,22,00,12,22,00,27,27,27,00,00,29,00,00,00,22,00,00,00,00,00],//12
-	[01,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,07,02,02,02,02,02,02],//13
+	[01,00,10,00,06,14,00,22,00,11,22,11,00,22,00,27,27,27,00,00,29,00,00,00,22,00,13,00,00,21],//12
+	[01,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,02,07,07,02,02,02,02,02,02],//13
 	[01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01,01] //14
 ];
 }
 else if (s==1){
 once=true;
-background.src = "./obj/background1.png";
+espawn = true;
 mapchip.src = "./obj/tile.png";
 map = [
 //   -0-----------------------------1-----------------------------2--------------------------
@@ -140,7 +130,7 @@ map = [
 }
 else if (s==2){
 once=true;
-background.src = "./obj/background1.png";
+espawn = true;
 mapchip.src = "./obj/tile.png";
 map = [
 //   -0-----------------------------1-----------------------------2--------------------------
@@ -164,7 +154,7 @@ map = [
 }
 else if (s==3){
 once=true;
-background.src = "./obj/background1.png";
+espawn = true;
 mapchip.src = "./obj/tile.png";
 map = [
 //   -0-----------------------------1-----------------------------2--------------------------
@@ -203,11 +193,11 @@ if (cnt == 0) {
 
 function upd(){
 if (once) {bgm.play()}
-ctx.drawImage(background,0,0,960,480);
-ctx.drawImage(enemy,ex,ey,32,32);
-ctx.drawImage(enemy2,ex2,ey2,32,32);
+if (screen==0) ctx.drawImage(background,0,0,960,480);
+if (screen==1) ctx.drawImage(background1,0,0,960,480);
 
 /*BLOCK*/
+if (once&&espawn) {enemyX = [];enemyY = [];enemyturn = [];enemyjump = [];enemyV = [];}
 var ladderX = new Array();//
 var ladderY = new Array();//
 var bridgeX = new Array();//
@@ -216,16 +206,14 @@ var spikeX = new Array();//
 var spikeY = new Array();//
 var blocksX = new Array();//
 var blocksY = new Array();//
+var slopeX = new Array();//
+var slopeY = new Array();//
 var waterX = new Array();//
 var waterY = new Array();//
 var sandX = new Array();//
 var sandY = new Array();//
 var jumperX = new Array();//
 var jumperY = new Array();//
-var ejumperX = new Array();
-var ejumperY = new Array();
-var ejumper2X = new Array();
-var ejumper2Y = new Array();
 var keyX = new Array();//
 var keyY = new Array();//
 var keyBX = new Array();//
@@ -246,6 +234,10 @@ for (var my=0; my<map.length; my++){for (var mx=0; mx<map[my].length; mx++){
 /*jumper*/ if (map[my][mx] === 7) {ctx.drawImage( mapchip, 96, 32, 32, 32, 32*mx, 32*my, 32, 32 );jumperX.push(mx*32);jumperY.push(my*32)}
 /*key*/ if (map[my][mx] === 8&&key) {ctx.drawImage( mapchip, 0, 64, 32, 32, 32*mx, 32*my, 32, 32 );keyX.push(mx*32);keyY.push(my*32)}
 /*keyB*/ if (map[my][mx] === 9&&!key) {ctx.drawImage( mapchip, 32, 64, 32, 32, 32*mx, 32*my, 32, 32 );keyBX.push(mx*32);keyBY.push(my*32)}
+/*player*/	if (map[my][mx] === 10&&once&&spawn) {x=32*mx, y=32*my;cx=x;cy=y}
+/*enemy*/ 	if (map[my][mx] === 11&&once&&espawn) {enemyX.push(mx*32);enemyY.push(my*32);enemyturn.push(true);enemyjump.push(true);enemyV.push(0);espeed.push(0.8);}
+/*-cp-*/	if (map[my][mx] === 13&&!cp==true) {ctx.drawImage( mapchip, 64, 32, 32, 32, 32*mx, 32*my, 32, 32 );cpX.push(mx*32);cpY.push(my*32)}
+/*slope*/ 	if (map[my][mx] === 14) {ctx.drawImage( mapchip, 128, 32, 32, 32, 32*mx, 32*my, 32, 32 );slopeX.push(mx*32);slopeY.push(my*32)}
 /*spikeA*/ if (map[my][mx] === 21) {ctx.drawImage( mapchip, 64, 64, 32, 32, 32*mx, 32*my, 32, 32 );spikeX.push(mx*32);spikeY.push(my*32)}
 /*blockA*/ if (map[my][mx] === 22) {ctx.drawImage( mapchip, 96, 64, 32, 32, 32*mx, 32*my, 32, 32 );blocksX.push(mx*32);blocksY.push(my*32)}
 /*spikeB*/ if (map[my][mx] === 23&&!key) {ctx.drawImage( mapchip, 128, 64, 32, 32, 32*mx, 32*my, 32, 32 );spikeX.push(mx*32);spikeY.push(my*32)}
@@ -256,16 +248,12 @@ for (var my=0; my<map.length; my++){for (var mx=0; mx<map[my].length; mx++){
 /*waterT*/ if (map[my][mx] === 28) {ctx.drawImage( mapchip, 128, 0, 32, 32, 32*mx, 32*my, 32, 32 );waterX.push(mx*32);waterY.push(my*32)}
 /*sandT*/ if (map[my][mx] === 29) {ctx.drawImage( mapchip, 224, 0, 32, 32, 32*mx, 32*my, 32, 32 );sandX.push(mx*32);sandY.push(my*32)}
 /*sandB*/ if (map[my][mx] === 30) {ctx.drawImage( mapchip, 192, 0, 32, 32, 32*mx, 32*my, 32, 32 );sandX.push(mx*32);sandY.push(my*32)}
-/*player*/	if (map[my][mx] === 10&&once&&spawn) {x=32*mx, y=32*my;cx=x;cy=y}
-/*enemy*/	if (map[my][mx] === 12&&once) {ex=32*mx, ey=32*my;}
-/*-cp-*/	if (map[my][mx] === 13&&!cp==true) {ctx.drawImage( mapchip, 64, 32, 32, 32, 32*mx, 32*my, 32, 32 );cpX.push(mx*32);cpY.push(my*32)}
-/*enemy2*/	if (map[my][mx] === 20&&once) {ex2=32*mx, ey2=32*my;}
 }}
 /*BLOCK*/
 
 
-if (inkey[39]&&inkey[16]) {x=x+xspeed+2;playerturn=0} else if (inkey[39]) {x=x+xspeed;playerturn=0}
-if (inkey[37]&&inkey[16]) {x=x-xspeed-2;playerturn=1} else if (inkey[37]) {x=x-xspeed;playerturn=1}
+if (inkey[39]) {x=x+xspeed;playerturn=0}
+if (inkey[37]) {x=x-xspeed;playerturn=1}
 if (inkey[32]&&!jump) {vy=-0.5;jump=true;isjump=true}
 if (isjump&&inkey[32]&&vy>-(jumpheight)) {vy=vy-0.20} else {isjump=false}
 jump=true
@@ -285,9 +273,6 @@ for (var i=0;i<cpY.length;i++) {
 	x<cpX[i]+hitboxX-2&&
 	x>cpX[i]-hitboxX+2
 	) {cp=true;savecp()}
-}
-
-for (var i=0;i<cpX.length;i++) {
 if (x>cpX[i]-hitboxY&&cpX[i]>x&&y>cpY[i]-hitboxY&&cpY[i]+hitboxY>y) {cp=true;savecp()}
 else if (x>cpX[i]&&cpX[i]+hitboxY>x&&y>cpY[i]-hitboxY&&cpY[i]+hitboxY>y) {cp=true;savecp()}
 }
@@ -314,12 +299,35 @@ for (var i=0;i<blocksY.length;i++) {
 	x<blocksX[i]+hitboxX-2&&
 	x>blocksX[i]-hitboxX+2
 	) {y=blocksY[i]+hitboxY;vy=0}
-}
-for (var i=0;i<blocksX.length;i++) {
 if (x>blocksX[i]-hitboxX&&blocksX[i]>x&&y>blocksY[i]-hitboxX&&blocksY[i]+hitboxX>y) {x=blocksX[i]-hitboxX}
 else if (x>blocksX[i]&&blocksX[i]+hitboxX>x&&y>blocksY[i]-hitboxX&&blocksY[i]+hitboxX>y) {x=blocksX[i]+hitboxX}
 }
 /*block-------------------------------------------------------------------------------------------*/
+
+/*slope*/
+for (var i=0;i<slopeY.length;i++) {
+	if (
+	!inkey[32]&&
+	y+hitboxY+0.00001>slopeY[i]&&
+	y<slopeY[i]&&
+	x<slopeX[i]+hitboxX-2&&
+	x>slopeX[i]-hitboxX+2
+	) {y=slopeY[i]-x+slopeX[i];jump=false;vy=0}
+	else if (
+	y+hitboxY>slopeY[i]&&
+	y<slopeY[i]&&
+	x<slopeX[i]+hitboxX-2&&
+	x>slopeX[i]-hitboxX+2
+	) {y=slopeY[i]-hitboxY;jump=false;vy=0}
+	else if (
+	y>slopeY[i]&&
+	y-hitboxY<slopeY[i]&&
+	x<slopeX[i]+hitboxX-2&&
+	x>slopeX[i]-hitboxX+2
+	) {y=slopeY[i]+hitboxY;vy=0}
+if (x>slopeX[i]&&slopeX[i]+hitboxX>x&&y>slopeY[i]-hitboxX&&slopeY[i]+hitboxX>y) {x=slopeX[i]+hitboxX}
+}
+/*slope-------------------------------------------------------------------------------------------*/
 
 /*spike*/
 for (var i=0;i<spikeY.length;i++) {
@@ -335,31 +343,67 @@ for (var i=0;i<spikeY.length;i++) {
 	x<spikeX[i]+hitboxX-2&&
 	x>spikeX[i]-hitboxX+2
 	) {game=0}
-}
-for (var i=0;i<spikeX.length;i++) {
 if (x>spikeX[i]-hitboxX&&spikeX[i]>x&&y>spikeY[i]-hitboxX&&spikeY[i]+hitboxX>y) {game=0}
 else if (x>spikeX[i]&&spikeX[i]+hitboxX>x&&y>spikeY[i]-hitboxX&&spikeY[i]+hitboxX>y) {game=0}
 }
 /*spike-------------------------------------------------------------------------------------------*/
+	
+/*enemy*/
+for (var i=0;i<enemyY.length;i++) {
+enemyjump[i]=true
+for (var j=0;j<blocksY.length;j++) {
+	if (
+	enemyY[i]+32.000001>blocksY[j]&&
+	enemyY[i]<blocksY[j]&&
+	enemyX[i]<blocksX[j]+31&&
+	enemyX[i]>blocksX[j]-31
+	) {enemyY[i]=blocksY[j]-32;enemyjump[i]=false;enemyV[i]=0}
+	else if (
+	enemyY[i]>blocksY[j]&&
+	enemyY[i]-32<blocksY[j]&&
+	enemyX[i]<blocksX[j]+31&&
+	enemyX[i]>blocksX[j]-31
+	) {enemyY[i]=blocksY[j]+32;enemyV[i]=0}
+if (enemyX[i]>blocksX[j]-32&&blocksX[j]>enemyX[i]&&enemyY[i]>blocksY[j]-32&&blocksY[j]+32>enemyY[i]) {if (enemyturn[i]==true){enemyturn[i]=false} else {enemyturn[i]=true}}
+else if (enemyX[i]>blocksX[j]&&blocksX[j]+32>enemyX[i]&&enemyY[i]>blocksY[j]-32&&blocksY[j]+32>enemyY[i]) {if (enemyturn[i]==true){enemyturn[i]=false} else {enemyturn[i]=true}}
+}
+	ctx.drawImage(enemy, enemyX[i], enemyY[i], 32, 32 );
+	if (
+	y+hitboxY>enemyY[i]&&
+	y<enemyY[i]&&
+	x<enemyX[i]+hitboxX-2&&
+	x>enemyX[i]-hitboxX+2
+	) {game=0}
+	else if (
+	y>enemyY[i]&&
+	y-hitboxY<enemyY[i]&&
+	x<enemyX[i]+hitboxX-2&&
+	x>enemyX[i]-hitboxX+2
+	) {game=0}
+if (x>enemyX[i]-hitboxX&&enemyX[i]>x&&y>enemyY[i]-hitboxX&&enemyY[i]+hitboxX>y) {game=0}
+else if (x>enemyX[i]&&enemyX[i]+hitboxX>x&&y>enemyY[i]-hitboxX&&enemyY[i]+hitboxX>y) {game=0}
+if (enemyjump[i]) {enemyY[i]=enemyY[i]+enemyV[i];enemyV[i]=enemyV[i]+0.05}
+if (enemyturn[i]) {enemyX[i]=enemyX[i]+espeed[i]} else if (!enemyturn[i]) {enemyX[i]=enemyX[i]-espeed[i]}
+}
+/*enemy-------------------------------------------------------------------------------------------*/
 
 /*bridge*/
 for (var i=0;i<bridgeY.length;i++) {
 	if (
 	!inkey[32]&&
 	y+hitboxY+0.00001>bridgeY[i]&&
-	y<bridgeY[i]&&
+	y+hitboxY-5<bridgeY[i]&&
 	x<bridgeX[i]+hitboxX-2&&
 	x>bridgeX[i]-hitboxX+2
 	) {y=bridgeY[i]-hitboxY;jump=false;vy=0}
 	else if (
 	y+hitboxY>bridgeY[i]&&
-	y<bridgeY[i]&&
+	y+hitboxY-5<bridgeY[i]&&
 	x<bridgeX[i]+hitboxX-2&&
 	x>bridgeX[i]-hitboxX+2
 	) {y=bridgeY[i]-hitboxY;jump=false;vy=0}
 }
 /*bridge-------------------------------------------------------------------------------------------*/
-
 
 /*ladder*/
 for (var i=0;i<ladderY.length;i++) {
@@ -394,8 +438,6 @@ for (var i=0;i<jumperY.length;i++) {
 	x<jumperX[i]+hitboxX-2&&
 	x>jumperX[i]-hitboxX+2
 	) {y--;vy=-5}
-}
-for (var i=0;i<jumperX.length;i++) {
 if (x>jumperX[i]-hitboxX&&jumperX[i]>x&&y>jumperY[i]-hitboxX&&jumperY[i]+hitboxX>y) {y--;vy=-5}
 else if (x>jumperX[i]&&jumperX[i]+hitboxX>x&&y>jumperY[i]-hitboxX&&jumperY[i]+hitboxX>y) {y--;vy=-5}
 }
@@ -408,17 +450,15 @@ for (var i=0;i<keyY.length;i++) {
 	y<keyY[i]&&
 	x<keyX[i]+hitboxX-2&&
 	x>keyX[i]-hitboxX+2
-	) {key=false}
+	) {key=false;getk.play()}
 	else if (
 	y>keyY[i]&&
 	y-hitboxY<keyY[i]&&
 	x<keyX[i]+hitboxX-2&&
 	x>keyX[i]-hitboxX+2
-	) {key=false}
-}
-for (var i=0;i<keyX.length;i++) {
-if (x>keyX[i]-hitboxX&&keyX[i]>x&&y>keyY[i]-hitboxX&&keyY[i]+hitboxX>y) {key=false}
-else if (x>keyX[i]&&keyX[i]+hitboxX>x&&y>keyY[i]-hitboxX&&keyY[i]+hitboxX>y) {key=false}
+	) {key=false;getk.play()}
+if (x>keyX[i]-hitboxX&&keyX[i]>x&&y>keyY[i]-hitboxX&&keyY[i]+hitboxX>y) {key=false;getk.play()}
+else if (x>keyX[i]&&keyX[i]+hitboxX>x&&y>keyY[i]-hitboxX&&keyY[i]+hitboxX>y) {key=false;getk.play()}
 }
 /*key-------------------------------------------------------------------------------------------*/
 
@@ -430,17 +470,15 @@ for (var i=0;i<keyBY.length;i++) {
 	y<keyBY[i]&&
 	x<keyBX[i]+hitboxX-2&&
 	x>keyBX[i]-hitboxX+2
-	) {key=true}
+	) {key=true;getk.play()}
 	else if (
 	y>keyBY[i]&&
 	y-hitboxY<keyBY[i]&&
 	x<keyBX[i]+hitboxX-2&&
 	x>keyBX[i]-hitboxX+2
-	) {key=true}
-}
-for (var i=0;i<keyBX.length;i++) {
-if (x>keyBX[i]-hitboxX&&keyBX[i]>x&&y>keyBY[i]-hitboxX&&keyBY[i]+hitboxX>y) {key=true}
-else if (x>keyBX[i]&&keyBX[i]+hitboxX>x&&y>keyBY[i]-hitboxX&&keyBY[i]+hitboxX>y) {key=true}
+	) {key=true;getk.play()}
+if (x>keyBX[i]-hitboxX&&keyBX[i]>x&&y>keyBY[i]-hitboxX&&keyBY[i]+hitboxX>y) {key=true;getk.play()}
+else if (x>keyBX[i]&&keyBX[i]+hitboxX>x&&y>keyBY[i]-hitboxX&&keyBY[i]+hitboxX>y) {key=true;getk.play()}
 }
 /*keyB-------------------------------------------------------------------------------------------*/
 
@@ -453,8 +491,6 @@ for (var i=0;i<waterY.length;i++) {
 	x<waterX[i]+hitboxX-2&&
 	x>waterX[i]-hitboxX+2
 	) {y=y-0.3;vy=-1;jump=false}
-}
-for (var i=0;i<waterX.length;i++) {
 if (inkey[32]&&x>waterX[i]-hitboxX&&waterX[i]>x&&y>waterY[i]-hitboxX&&waterY[i]+hitboxX>y) {y=y-0.3;vy=-1;jump=false}
 else if (inkey[32]&&x>waterX[i]&&waterX[i]+hitboxX>x&&y>waterY[i]-hitboxX&&waterY[i]+hitboxX>y) {y=y-0.3;vy=-1;jump=false}
 }
@@ -469,8 +505,6 @@ for (var i=0;i<sandY.length;i++) {
 	x<sandX[i]+hitboxX-2&&
 	x>sandX[i]-hitboxX+2
 	) {vy=1;jump=false}
-}
-for (var i=0;i<sandX.length;i++) {
 if (inkey[32]&&x>sandX[i]-hitboxX&&sandX[i]>x&&y>sandY[i]-hitboxX&&sandY[i]+hitboxX>y) {vy=1;jump=false}
 else if (inkey[32]&&x>sandX[i]&&sandX[i]+hitboxX>x&&y>sandY[i]-hitboxX&&sandY[i]+hitboxX>y) {vy=1;jump=false}
 }
@@ -480,103 +514,19 @@ if (jump) {y=y+vy;vy=vy+0.05}
 
 once=false;
 spawn=false;
+espawn=false;
 
-
-
-
-/*Eblock*/
-ejump=true
-for (var i=0;i<blocksY.length;i++) {
-	if (
-	ey+32.001>blocksY[i]&&
-	ey<blocksY[i]&&
-	ex<blocksX[i]+30&&
-	ex>blocksX[i]-30
-	) {ey=blocksY[i]-32;ejump=false;evy=0}
-	else if (
-	ey>blocksY[i]&&
-	ey-32<blocksY[i]&&
-	ex<blocksX[i]+30&&
-	ex>blocksX[i]-30
-	) {ey=blocksY[i]+32;evy=0}
-}
-
-for (var i=0;i<blocksX.length;i++) {
-if (ex>blocksX[i]-32&&blocksX[i]>ex&&ey>blocksY[i]-32&&blocksY[i]+32>ey) {if (turn==1){turn=0} else {turn=1}}
-else if (ex>blocksX[i]&&blocksX[i]+32>ex&&ey>blocksY[i]-32&&blocksY[i]+32>ey) {if (turn==1){turn=0} else {turn=1}}
-}
-/*Eblock*/
-
-/*Eblock2*/
-ejump2=true
-for (var i=0;i<blocksY.length;i++) {
-	if (
-	ey2+32.001>blocksY[i]&&
-	ey2<blocksY[i]&&
-	ex2<blocksX[i]+30&&
-	ex2>blocksX[i]-30
-	) {ey2=blocksY[i]-32;ejump2=false}
-	else if (
-	ey2>blocksY[i]&&
-	ey2-32<blocksY[i]&&
-	ex2<blocksX[i]+30&&
-	ex2>blocksX[i]-30
-	) {ey2=blocksY[i]+32;evy=0}
-}
-
-for (var i=0;i<blocksX.length;i++) {
-if (ex2>blocksX[i]-32&&blocksX[i]>ex2&&ey2>blocksY[i]-32&&blocksY[i]+32>ey2) {if (turn2==1){turn2=0} else {turn2=1}}
-else if (ex2>blocksX[i]&&blocksX[i]+32>ex2&&ey2>blocksY[i]-32&&blocksY[i]+32>ey2) {if (turn2==1){turn2=0} else {turn2=1}}
-}
-/*Eblock2*/
-
-
-/*enemy*/
-if (
-y+hitboxY>ey&&
-y<ey&&
-x<ex+hitboxX-2&&
-x>ex-hitboxY+2
-) {game=0}
-else if (
-y>ey&&
-y-hitboxY<ey&&
-x<ex+hitboxY-2&&
-x>ex-hitboxY+2
-) {game=0}
-if (x>ex-hitboxY&&ex>x&&y>ey-hitboxY&&ey+hitboxY>y) {game=0}
-else if (x>ex&&ex+hitboxY>x&&y>ey-hitboxY&&ey+hitboxY>y) {game=0}
-/*enemy*/
-
-/*enemy2*/
-if (
-y+hitboxY>ey2&&
-y<ey2&&
-x<ex2+hitboxX-2&&
-x>ex2-hitboxX+2
-) {game=0}
-else if (
-y>ey2&&
-y-hitboxY<ey2&&
-x<ex2+hitboxX-2&&
-x>ex2-hitboxX+2
-) {game=0}
-if (x>ex2-hitboxY&&ex2>x&&y>ey2-hitboxY&&ey2+hitboxY>y) {game=0}
-else if (x>ex2&&ex2+hitboxY>x&&y>ey2-hitboxY&&ey2+hitboxY>y) {game=0}
-/*enemy2*/
-
-if (ejump) {ey=ey+evy;evy=evy+0.05}
-if (turn==1) {ex=ex+espeed} else if (turn==0) {ex=ex-espeed}
-if (ejump2) {ey2=ey2+evy2;evy2=evy2+0.05}
-if (turn2==1) {ex2=ex2+espeed2} else if (turn2==0) {ex2=ex2-espeed2}
 if (y>480) {game=0;}
-
-
-
 
 if (game==0) {loadcp();deathcnt++;document.getElementById("death").innerHTML = "death:"+deathcnt}
 
-if (game==1) {window.requestAnimationFrame(upd)};
+
+function frm() {
+frame++
+console.log(frame);document.getElementById("framecnt").innerHTML = "frame["+frame+"]";
+if (game==1) {window.requestAnimationFrame(upd)}
+}
+setTimeout(frm,1);
 
 if (x>930&&3>screen) {screen++;x=0;makemap(screen)} else if (x>930){game=2}
 if (x<0&&0<screen) {screen--;x=930;makemap(screen)} else if (x<0) {x=0}
@@ -588,46 +538,41 @@ if (game==2) {bgm.pause();clear.play();document.getElementById("TEXT").innerHTML
 
 function savecp() {
 spawn=false;
+espawn=false;
 	getc.play()
 	cx = x;
 	cy = y;
+	cvy = vy;
 	ck = key;
 	cc = cnt;
 	cg = game;
 	co = once;
 	cs = screen;
-	cex = ex;
-	cey = ey;
+
+	cejump = enemyjump;
+	ceturn = enemyturn;
+	cex = enemyX;
+	cey = enemyY;
+	cev = enemyV;
 	ces = espeed;
-	cet = turn;
-	cevy = evy;
-	cex2 = ex2;
-	cey2 = ey2;
-	ces2 = espeed2;
-	cet2 = turn2;
-	cevy = evy;
-	cevy2 = evy2;
+
 }
 
 function loadcp(){
 	x = cx;
 	y = cy;
+	vy = cvy;
 	key = ck;
 	cnt = cc;
 	game = cg;
 	once = co;
 	screen = cs;
-	ex = cex;
-	ey = cey;
-	espeed = ces;
-	turn = cet;
-	evy = cevy;
-	ex2 = cex2;
-	ey2 = cey2;
-	espeed2 = ces2;
-	turn2 = cet2;
-	evy = cevy;
-	evy2 = cevy2;
+	enemyjump = cejump
+	enemyturn = ceturn
+	enemyX = cex
+	enemyY = cey
+	enemyV = cev
+	espeed = ces
 	document.getElementById("TEXT").innerHTML = "";
 	game = 1;
 	makemap(screen)
