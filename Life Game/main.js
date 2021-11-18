465d1392a3bc7170e1305a57455378a82001921a
nimsite <91060664+nimsite2@users.noreply.github.com>
2021-11-18 20:07:07 +0900

    up

--- Life Game/main.js
+++ Life Game/main.js
@@ -7,8 +7,31 @@ function keyu(e) {inkey[e.keyCode] = false}
 function chli() {
 	w = document.getElementById("width").value
 	h = document.getElementById("height").value
-	window.location.href = 'index.html?w='+w+'&h='+h;
+	px = document.getElementById("px").value
+	window.location.href = 'index.html?w='+w+'&h='+h+'&px='+px;
 }
+var color = "lime"
+document.getElementById("color").style.backgroundColor = color
+function chcolor() {
+	if (color=="lime") {color="red"} else {color = "lime"}
+	document.getElementById("color").style.backgroundColor = color
+}
+
+
+document.getElementById("twop").checked = false;
+var battle = false;
+function twop() {
+	if (!battle) {battle=true} else {battle=false}
+}
+
+
+
+var seru = document.getElementsByTagName('th');
+for (var i=0;i<seru.length;i++){
+seru[i].style.width = px+'px';
+seru[i].style.height= px+'px';
+}
+
 
 var inmou = false
 window.addEventListener("mousedown",moud)
@@ -18,10 +41,10 @@ function mouu() {inmou=false}
 
 function tclick(i,j) {
 var blocks = document.getElementById(i+":"+j)
-if (blocks.style.backgroundColor=='lime') {
+if (blocks.style.backgroundColor==color) {
 blocks.style.backgroundColor = 'black';
 } else {
-blocks.style.backgroundColor = 'lime';
+blocks.style.backgroundColor = color;
 }
 }
 
@@ -34,24 +57,24 @@ var snj = new Array();
 for (var i=0;i<w;i++){
 	for (var j=0;j<h;j++) {
 		var blocks = document.getElementById(i+":"+j)
-		if (blocks.style.backgroundColor == 'lime') {
+		if (blocks.style.backgroundColor == color) {
 			var blocnt = 0;
 			var blocks1 = document.getElementById(String(parseInt(i+1))+":"+j)
-			if (blocks1!=null&&blocks1.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks1!=null&&blocks1.style.backgroundColor == color) {blocnt++}
 			var blocks2 = document.getElementById(String(parseInt(i-1))+":"+j)
-			if (blocks2!=null&&blocks2.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks2!=null&&blocks2.style.backgroundColor == color) {blocnt++}
 			var blocks3 = document.getElementById(i+":"+String(parseInt(j+1)))
-			if (blocks3!=null&&blocks3.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks3!=null&&blocks3.style.backgroundColor == color) {blocnt++}
 			var blocks4 = document.getElementById(i+":"+String(parseInt(j-1)))
-			if (blocks4!=null&&blocks4.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks4!=null&&blocks4.style.backgroundColor == color) {blocnt++}
 			var blocks5 = document.getElementById(String(parseInt(i+1))+":"+String(parseInt(j+1)))
-			if (blocks5!=null&&blocks5.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks5!=null&&blocks5.style.backgroundColor == color) {blocnt++}
 			var blocks6 = document.getElementById(String(parseInt(i-1))+":"+String(parseInt(j-1)))
-			if (blocks6!=null&&blocks6.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks6!=null&&blocks6.style.backgroundColor == color) {blocnt++}
 			var blocks7 = document.getElementById(String(parseInt(i-1))+":"+String(parseInt(j+1)))
-			if (blocks7!=null&&blocks7.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks7!=null&&blocks7.style.backgroundColor == color) {blocnt++}
 			var blocks8 = document.getElementById(String(parseInt(i+1))+":"+String(parseInt(j-1)))
-			if (blocks8!=null&&blocks8.style.backgroundColor == 'lime') {blocnt++}
+			if (blocks8!=null&&blocks8.style.backgroundColor == color) {blocnt++}
 			if (blocnt<=1||blocnt>=4){
 				sni.push(i)
 				snj.push(j)
@@ -63,21 +86,21 @@ for (var i=0;i<w;i++){
 		else {
 			var blocnt2 = 0;
 			var blocks21 = document.getElementById(String(parseInt(i+1))+":"+j)
-			if (blocks21!=null&&blocks21.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks21!=null&&blocks21.style.backgroundColor == color) {blocnt2++}
 			var blocks22 = document.getElementById(String(parseInt(i-1))+":"+j)
-			if (blocks22!=null&&blocks22.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks22!=null&&blocks22.style.backgroundColor == color) {blocnt2++}
 			var blocks23 = document.getElementById(i+":"+String(parseInt(j+1)))
-			if (blocks23!=null&&blocks23.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks23!=null&&blocks23.style.backgroundColor == color) {blocnt2++}
 			var blocks24 = document.getElementById(i+":"+String(parseInt(j-1)))
-			if (blocks24!=null&&blocks24.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks24!=null&&blocks24.style.backgroundColor == color) {blocnt2++}
 			var blocks25 = document.getElementById(String(parseInt(i+1))+":"+String(parseInt(j+1)))
-			if (blocks25!=null&&blocks25.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks25!=null&&blocks25.style.backgroundColor == color) {blocnt2++}
 			var blocks26 = document.getElementById(String(parseInt(i-1))+":"+String(parseInt(j-1)))
-			if (blocks26!=null&&blocks26.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks26!=null&&blocks26.style.backgroundColor == color) {blocnt2++}
 			var blocks27 = document.getElementById(String(parseInt(i-1))+":"+String(parseInt(j+1)))
-			if (blocks27!=null&&blocks27.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks27!=null&&blocks27.style.backgroundColor == color) {blocnt2++}
 			var blocks28 = document.getElementById(String(parseInt(i+1))+":"+String(parseInt(j-1)))
-			if (blocks28!=null&&blocks28.style.backgroundColor == 'lime') {blocnt2++}
+			if (blocks28!=null&&blocks28.style.backgroundColor == color) {blocnt2++}
 			if (blocnt2==3) {
 				si.push(i)
 				sj.push(j)
@@ -87,12 +110,13 @@ for (var i=0;i<w;i++){
 	}
 for (var i=0;i<si.length;i++){
 	var blocks = document.getElementById(si[i]+":"+sj[i])
-	blocks.style.backgroundColor = "lime"
+	blocks.style.backgroundColor = color
 }
 for (var i=0;i<sni.length;i++){
 	var blocks = document.getElementById(sni[i]+":"+snj[i])
 	blocks.style.backgroundColor = "black"
 }
+if(battle){chcolor()};
 if (loop) {setTimeout(start,50)}
 }
 
