d1e39f20a208eb35bde5dd21d7545afc5c46d7d8
nimsite <91060664+nimsite2@users.noreply.github.com>
2021-12-03 02:36:07 +0900

    Update x.js

--- x.js
+++ x.js
@@ -1,9 +1,1 @@
-function X(){
-if(!codeedit){
-document.getElementsByTagName("html")[0].innerHTML=`<textarea style='background-color:black;color:lime;border:none;width:100%;height:90vh;margin:0px;padding:2px;'>`+document.documentElement.outerHTML.replace(/&/g,'&amp;').replace(/"/g,'&quot;').replace(/'/g,'&#039;').replace(/</g,'&lt;').replace(/>/g,'&gt;');+`</textarea>`
-codeedit = true;
-} else if (codeedit){
-document.getElementsByTagName("html")[0].innerHTML=document.getElementsByTagName("textarea")[0].value
-codeedit = false;
-}
-}
+if(!codeedit){document.getElementsByTagName("html")[0].innerHTML=`<textarea style='background-color:black;color:lime;border:none;width:100%;height:90vh;margin:0px;padding:2px;'>`+document.documentElement.outerHTML.replace(/&/g,'&amp;').replace(/"/g,'&quot;').replace(/'/g,'&#039;').replace(/</g,'&lt;').replace(/>/g,'&gt;');+`</textarea>`;var codeedit = true;}else{document.getElementsByTagName("html")[0].innerHTML=document.getElementsByTagName("textarea")[0].value;var codeedit = false;}
