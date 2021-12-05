a07d98cd5ee6434795a14ddc688d7af108889abd
nimsite <91060664+nimsite2@users.noreply.github.com>
2021-12-06 00:39:33 +0900

    up

--- textandimgstyle/main.js
+++ textandimgstyle/main.js
@@ -1,7 +1,7 @@
-document.getElementById("Sam").style.color=document.getElementById("inputcolor").value
-document.getElementById("Sam").style.fontSize=document.getElementById("inputtext").value
-document.getElementById("SamImg").style.width=document.getElementById("imgw").value
-document.getElementById("SamImg").style.height=document.getElementById("imgh").value
+document.getElementsByTagName("h2")[0].style.color=document.getElementById("inputcolor").value
+document.getElementsByTagName("h2")[0].style.fontSize=document.getElementById("inputtext").value
+document.getElementsByTagName('img')[0].style.width=document.getElementById("imgw").value
+document.getElementsByTagName('img')[0].style.height=document.getElementById("imgh").value
 
 
 
