3b1cff2de738d4b8aa203a9f5a5ab25bc186d0e1
nimsite <91060664+nimsite2@users.noreply.github.com>
2021-12-06 00:30:20 +0900

    a

---
+++ textandimgstyle/main.js
@@ -0,0 +1,7 @@
+document.getElementById("Sam").style.color=document.getElementById("inputcolor").value
+document.getElementById("Sam").style.fontSize=document.getElementById("inputtext").value
+document.getElementById("SamImg").style.width=document.getElementById("imgw").value
+document.getElementById("SamImg").style.height=document.getElementById("imgh").value
+
+
+
